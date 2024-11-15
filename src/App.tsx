2c88d4dd8c8f1fe8635bb41233c5c4c3021b3dd7
Vlad <cosadesgo@gmail.com>
2024-11-15 17:11:13 +0300

    add monaco editor

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import * as esbuild from "esbuild-wasm";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from "./components/code-editor";
 
 export const App = () => {
   const [input, setInput] = useState("");
@@ -61,6 +62,7 @@ export const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
